0dfa03b44406864ef4ec1b515d26bed46046c6a6
mironov <aleksandar.n.mironov@gmail.com>
2017-09-29 01:03:13 +0300

    IMG upload is working now :)

--- Janta.bg/app/src/main/java/com/example/ivan/jantabg/AddOfferActivity.java
+++ Janta.bg/app/src/main/java/com/example/ivan/jantabg/AddOfferActivity.java
@@ -1,7 +1,9 @@
 package com.example.ivan.jantabg;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -9,6 +11,7 @@ import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,7 +35,38 @@ public class AddOfferActivity extends AppCompatActivity {
         db = DataBaseHelper.getHelper(this);
         userMail = getIntent().getExtras().getString("userMail");
         setOnAddBtnListener();
+        verifyStoragePermissions(this);
     }
+
+    /////////////////////// this is needed for API 23+ to permit permission !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE, //api 16
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+    /////////////////////////
 
     public void setOnAddBtnListener() {
         final EditText title = (EditText) findViewById(R.id.add_offer_title);
@@ -86,7 +120,7 @@ public class AddOfferActivity extends AppCompatActivity {
                 String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
                 // Get the cursor
-                Cursor cursor = getContentResolver().query(selectedImage,
+                Cursor cursor = this.getContentResolver().query(selectedImage,
                         filePathColumn, null, null, null);
                 // Move to first row
                 cursor.moveToFirst();
@@ -96,6 +130,7 @@ public class AddOfferActivity extends AppCompatActivity {
                 cursor.close();
                 ImageView imgView = (ImageView) findViewById(R.id.add_offer_imageview);
                 // Set the Image in ImageView after decoding the String
+
                 imgView.setImageBitmap(BitmapFactory
                         .decodeFile(imgDecodableString));
 
